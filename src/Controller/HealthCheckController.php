3dff2ea6f56d20e464c063fecd803c2ac2380a3b
damworks <larry.chiarello@gmail.com>
2025-09-01 18:35:12 +0200

    add logging to health check endpoint

--- src/Controller/HealthCheckController.php
+++ src/Controller/HealthCheckController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use Pimcore\Bundle\ApplicationLoggerBundle\ApplicationLogger;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HealthCheckController extends AbstractController
 {
     #[Route('/health_check', name: 'app_health_check', methods: ['GET'])]
-    public function __invoke(): JsonResponse
+    public function __invoke(
+        Request $request,
+        ApplicationLogger $applicationLogger
+    ): JsonResponse
     {
+        $ip = $request->getClientIp();
+        $applicationLogger->info('Health check endpoint from IP: ' . $ip);
+        //$applicationLogger->error('ERROR');
+        //$applicationLogger->debug('Test Debug', ['debugTest'=>'debugValue']);
         return $this->json(['status' => 'ok']);
     }
 }
